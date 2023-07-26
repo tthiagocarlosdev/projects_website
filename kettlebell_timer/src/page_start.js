73fa4e615c35001c14240b77926fe92346379a09
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-07-26 08:04:24 -0300

    kettlebell_timer: cronometro e layout da page-start construido

--- kettlebell_timer/src/page_start.js
+++ kettlebell_timer/src/page_start.js
@@ -10,8 +10,40 @@ window.onload = function() {
     const tempoDaProva = document.querySelector('.tempo_da_prova');
     console.log(dados)
 
-    nomeDoAtleta.innerHTML = `Nome do Atleta: ${dados.athleteName}`;
-    nomeDaProva.innerHTML = `Nome da Prova: ${dados.athleteTest}`;
-    pesoDoKettlebell.innerHTML = `Peso da Prova: ${dados.kettlebellWeight} kg`;
-    tempoDaProva.innerHTML = `Tempo da prova: ${dados.timeTest} min`
-}
+    document.getElementById('show_athlete_name').innerHTML = `Nome do Atleta: ${dados.athleteName}`;
+    document.getElementById('show_athleteTest').innerHTML = `Prova: ${dados.athleteTest}`;
+    document.getElementById('show_kettlebellWeight').innerHTML = `Peso Kettlebell ${dados.kettlebellWeight} kg`;
+
+    // CLOCK - CLOCK - CLOCK - CLOCK - CLOCK - CLOCK - CLOCK - CLOCK - CLOCK - CLOCK - CLOCK - CLOCK
+    // Define a quantidade de minutos que o cronômetro deve contar
+    const minutos = dados.timeTest;
+    console.log(`minutos: ${minutos}`);
+
+    // Converte os minutos em segundos
+    let segundos = minutos * 60;
+
+    // Define uma função que atualiza o cronômetro a cada segundo
+    const atualizarCronometro = () => {
+    // Converte os segundos em minutos e segundos
+    const minutosExibicao = Math.floor(segundos / 60).toString().padStart(2, "0");
+    const segundosExibicao = (segundos % 60).toString().padStart(2, "0");
+
+    // Atualiza o texto do cronômetro
+    document.getElementById('minutos').innerHTML = minutosExibicao;
+    document.getElementById('segundos').innerHTML = segundosExibicao;
+
+    // Decrementa o número de segundos restantes
+    segundos--;
+
+    // Para o cronômetro quando atingir zero
+    if (segundos < 0) {
+        clearInterval(intervalId);
+    }
+    };
+
+    // Adicione um evento de clique ao botão
+    document.getElementById('startButton').addEventListener('click', () => {
+    // Chama a função atualizarCronometro a cada segundo
+    const intervalId = setInterval(atualizarCronometro, 1000);
+    });
+};
