0b207a6913f47125621902d34b6c8cd8aa2bf6b8
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-08-04 11:54:21 -0300

    kettlebell_timer: estilizacao da pagina index, formulario criado na pagina index e configurado

--- kettlebell_timer/script.js
+++ kettlebell_timer/script.js
@@ -1,23 +1,81 @@
-function registrationFunction(){
-    const athlete_name = document.querySelector('#athleteName');
-    const athlete_test  = document.querySelector('#athleteTest');
-    const kettlebell_weight = document.querySelector('#kettlebellWeight');
-    const time_test = document.querySelector('#timeTest');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  checkInputs()
+})
 
-    const dataAthlete = {
-        athleteName: athlete_name.value.toUpperCase(),
-        athleteTest: athlete_test.value.toUpperCase(),
-        kettlebellWeight: kettlebell_weight.value,
-        timeTest: time_test.value,
-    };
+function checkInputs(){
 
-    if(dataAthlete.athleteName.length == 0 || 
-        dataAthlete.athleteTest.length == 0 || 
-        dataAthlete.kettlebellWeight.length == 0 ||
-        dataAthlete.timeTest.length == 0){
-        alert('Preencha todos os dados');
-    }else{
-        const dados_str = JSON.stringify(dataAthlete);
-        window.location.href = "./pages/page_start.html?dataAthlete=" + encodeURIComponent(dados_str);
-    }
+  const form = document.querySelector('form#form')
+  const athleteName = document.querySelector('input#athleteName')
+  const athleteTest = document.querySelector('select#athleteTest')
+  const kettlebellWeight = document.querySelector('select#kettlebellWeight')
+  const timeTest = document.querySelector('select#timeTest')
+
+  const dataAthlete = {
+    athleteName: athleteName.value.toUpperCase(),
+    athleteTest: athleteTest.value.toUpperCase(),
+    kettlebellWeight: kettlebellWeight.value,
+    timeTest: timeTest.value,
+  }
+  console.log(dataAthlete)
+
+  if (dataAthlete.athleteName === ''){
+    setErrorFor(athleteName, 'O nome do atleta é obrigatório!')
+  } else {
+    setSuccessFor(athleteName,  'teste')
+  }
+
+  if (dataAthlete.athleteTest === ''){
+    setErrorFor(athleteTest, 'A prova é obrigatória!')
+  } else {
+    setSuccessFor(athleteTest)
+  }
+
+  if (dataAthlete.kettlebellWeight === ''){
+    setErrorFor(kettlebellWeight, 'O peso do kettlebell é obrigatório!')
+  } else {
+    setSuccessFor(kettlebellWeight)
+  }
+
+  if (dataAthlete.timeTest === ''){
+    setErrorFor(timeTest, 'O tempo da prova é obrigatório!')
+  } else {
+    setSuccessFor(timeTest)
+  }
+
+  const formControls = form.querySelectorAll('.form-control')
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success"
+  })
+
+  if(formIsValid){
+    console.log('O fomulário está 100% válido!')
+    const data_str = JSON.stringify(dataAthlete);
+    window.location.href = "./pages/page_start.html?dataAthlete=" + encodeURIComponent(data_str);
+  }
+
+  
+}
+
+function setErrorFor(input, message){
+  const formControl = input.parentElement
+  const small = formControl.querySelector('small')
+
+  // Add message error
+  small.innerText = message
+
+  // Add class error
+  formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input){
+  const formControl = input.parentElement
+  const small = formControl.querySelector('small')
+
+  // Add message success
+  small.innerText = ''
+
+  // Add class success
+  formControl.className = 'form-control success'
 }
