0401e1efda77c35dccfb2db1310f468858fc02b6
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-09-04 16:14:33 -0300

    relatorio_de_servicos_externos: added function updateTechniciansList()and update function removeTechnical()

--- relatorio_de_servicos_externos/src/script.js
+++ relatorio_de_servicos_externos/src/script.js
@@ -2,10 +2,52 @@ import { options } from './unidades_policiais.js';
 
 const form = document.getElementById('form');
 
+const addTechniciansButton = document.getElementById('addTechniciansButton');
+addTechniciansButton.addEventListener('click', addTechnicians);
+let lista = document.querySelector('#list_of_technicians');
+
+let technical = {
+    names:[],
+    enrollment:[],
+}
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     toView()
 });
+
+
+//functions declarations
+function dateTime(value) {
+    const formatter = new Intl.DateTimeFormat("pt-br", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+    });
+
+    if (value.length === 0) {
+        // Código para retornar a data, hora e dia da semana atuais
+        const computerDate = new Date();
+        return formatter.format(computerDate);
+
+    } else {
+        // Código para retornar a data, hora e dia da semana a partir do valor fornecido
+        const dateObject = new Date(value);
+        return formatter.format(dateObject);
+    }
+}
+
+//função que testa se o valor digitado já se encontra na lista.
+function containsInTheList(value, list){
+    if(list.indexOf(value) != -1){
+        return true
+    } else {
+        return false
+    }
+}
 
 function loadOptions() {
     const optionsList = document.getElementById('options-list');
@@ -17,21 +59,36 @@ function loadOptions() {
     });
 }
 
-loadOptions(); // Chama a função para carregar as opções quando a página carrega
+function updateTechniciansList() {
+    let content = '';
+    for (let i = 0; i < technical.names.length; i++) {
+        content += `<p class="list-technical-and-registration">
+        <span>Nome:</span> ${technical.names[i]},
+        <span>Matrícula:</span> ${technical.enrollment[i]}
+        <a class="remove-link" data-index="${i}">
+            <i class="fa-solid fa-rectangle-xmark"></i>
+        </a>
+        </p>`;
+    }
 
-let technical = {
-    names:[],
-    enrollment:[],
+    // Atualize o conteúdo da lista
+    lista.innerHTML = content;
+
+    // Adicione event listeners aos elementos recém-criados
+    const removeLinks = document.querySelectorAll('.remove-link');
+    removeLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            const indexToRemove = parseInt(event.target.getAttribute('data-index'));
+            removeTechnical(indexToRemove);
+        });
+    });
 }
 
-const addTechniciansButton = document.getElementById('addTechniciansButton');
-addTechniciansButton.addEventListener('click', addTechnicians);
-
-
 function addTechnicians(){
+    console.log(technical)
     let name = document.querySelector('#name_technician').value;
     let registration = document.querySelector('#registration_technician').value;
-    let lista = document.querySelector('#list_of_technicians')    
+    
     const regex_registration = /(^[0-9]{3}.[0-9]{3}-[0-9])/
 
     if(containsInTheList(name, technical.names) || containsInTheList(registration, technical.enrollment)){
@@ -45,29 +102,21 @@ function addTechnicians(){
     } else {
         technical.names.push(name);
         technical.enrollment.push(registration);
+        // Após adicionar, atualize a lista
+        updateTechniciansList();
     }
     
-    let content = '';
-    for (let i = 0; i < technical.names.length; i++) {
-        content += `<p class="list-technical-and-registration" > 
-        <span>Nome:</span> ${technical.names[i]}, 
-        <span>Matrícula:</span> ${technical.enrollment[i]} 
-        <a onclick="removeTechnical(this,${i})"> 
-        <i class="fa-solid fa-rectangle-xmark"></i> 
-        </a> </p> <br>`;
-        lista.innerHTML = content;
-    }
 }
 
-function removeTechnical(item, index) {
-    const technicalItem = item.parentElement;
-    technicalItem.remove();
+function removeTechnical(index) {
     technical.names.splice(index, 1); // remove o nome correspondente
     technical.enrollment.splice(index, 1); // remove a matrícula correspondente
-    console.log(technical)
+
+    // Atualize a lista após a remoção
+    updateTechniciansList();
 }
 
-
+loadOptions(); // Chama a função para carregar as opções quando a página carrega
 function toView(){
     const pageData = {}
 
@@ -105,34 +154,3 @@ function toView(){
     }
 }
 
-//functions declarations
-function dateTime(value) {
-    const formatter = new Intl.DateTimeFormat("pt-br", {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric"
-    });
-
-    if (value.length === 0) {
-        // Código para retornar a data, hora e dia da semana atuais
-        const computerDate = new Date();
-        return formatter.format(computerDate);
-
-    } else {
-        // Código para retornar a data, hora e dia da semana a partir do valor fornecido
-        const dateObject = new Date(value);
-        return formatter.format(dateObject);
-    }
-}
-
-//função que testa se o valor digitado já se encontra na lista.
-function containsInTheList(value, list){
-    if(list.indexOf(value) != -1){
-        return true
-    } else {
-        return false
-    }
-}
