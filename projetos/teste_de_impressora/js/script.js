a0512a9a989ac75670ce20f8ddeba8b4f866172e
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-04-04 16:34:38 -0300

    teste_de_impressora v.1.0

--- projetos/teste_de_impressora/js/script.js
+++ projetos/teste_de_impressora/js/script.js
@@ -1,14 +1,12 @@
-$(document).ready(function() {
-    generateLorem();
-});
 
-const lorem = document.querySelector('#lorem');
 const dataAtual = document.querySelector('.data_atual');
 
+// Função para formatar a primeira letra em maiúsculo
 function primeiraLetraEmMaiusculo(texto) {
     return texto.charAt(0).toUpperCase() + texto.slice(1);
 }
 
+// Função para obter a data atual formatada
 function dataHora() {
     const formatter = new Intl.DateTimeFormat("pt-br", {
         weekday: "long",
@@ -27,12 +25,27 @@ function dataHora() {
 dataAtual.innerHTML = dataHora();
 console.log(dataAtual);
 
-function generateLorem() {
-    const textoDataHora = dataHora();
-    $(lorem).BaconIpsum({
-        type:'all-meat', 
-        paras:2, 
-        start_with_lorem:true,
-        after_paragraph: textoDataHora,
+ // Função para criar o elemento <p> com o texto "Teste de impressora" e um <span> com a data atualizada
+ function criarElementos() {
+    const texto = 'Teste de impressora';
+    const span = `<span class="data_atual">${dataHora()}</span>`;
+    let conteudo = '';
+
+    // Criar o conteúdo a ser inserido
+    for (let i = 0; i < 12; i++) {
+        conteudo += `${texto} - ${span}; `;
+    }
+
+    // Selecionar todos os elementos com a classe .secao_texto_data
+    const secoesTextoData = document.querySelectorAll('.secao_texto_data');
+
+    // Iterar sobre cada elemento e inserir o conteúdo
+    secoesTextoData.forEach(function(secao) {
+        const p = `<p>${conteudo}</p>`;
+        secao.insertAdjacentHTML('afterbegin', p);
     });
 }
+
+
+// Chamar a função ao carregar a página
+window.addEventListener('load', criarElementos);
